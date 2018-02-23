20b47c2d1856c0b905cbc5d633f51b46d2330120
Wilson da Rocha França <wilsonrf@gmail.com>
2018-02-23 00:34:41 -0300

    Basic front-end setup

--- src/main/java/com/wilsonfranca/currencyconverter/currency/CurrencyController.java
+++ src/main/java/com/wilsonfranca/currencyconverter/currency/CurrencyController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CurrencyController {
 
-    @RequestMapping(produces = {"text/html"}, value = "/")
+    @RequestMapping(value = "/")
     public String index() {
-        return "index.html";
+        return "index";
     }
 
 }
