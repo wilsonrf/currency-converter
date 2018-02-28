46c5f62c9fa7832b3f2305d5ab24a50c2ccfb756
Wilson da Rocha França <wilsonrf@gmail.com>
2018-02-28 18:18:15 -0300

    Signup form

--- src/main/java/com/wilsonfranca/currencyconverter/auth/AuthController.java
+++ src/main/java/com/wilsonfranca/currencyconverter/auth/AuthController.java
@@ -2,7 +2,11 @@ package com.wilsonfranca.currencyconverter.auth;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 /**
  * Created by wilson on 24/02/18.
@@ -19,5 +23,17 @@ public class AuthController {
     public String logout(Model model) {
         model.addAttribute("error", true);
         return "security/login";
+    }
+
+    @GetMapping(value = "signup.html")
+    public String signup(Model model) {
+
+        return "security/signup";
+    }
+
+    @PostMapping (value = "signup.html")
+    public String signup(@Valid SignupFormData signupFormData, Model model) {
+
+        return "security/signup";
     }
 }
---
+++ src/main/java/com/wilsonfranca/currencyconverter/auth/SignupFormData.java
@@ -0,0 +1,61 @@
+package com.wilsonfranca.currencyconverter.auth;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by wilson.franca on 28/02/18.
+ */
+public class SignupFormData {
+
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+
+    @NotNull
+    @NotEmpty
+    @Email
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
